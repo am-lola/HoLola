cf21a1b2887255f6005667ce58bdd9ed735c3fce
Tamas <t.bates@tum.de>
2017-06-28 01:46:38 +0200

    Capsule mesh & orientation script This addresses most of the capsule needs from issue #2 Capsules are represented as a skeletal mesh, with each end-cap being tied to a bone. This gives us a separate transform for each cap in Unity, so they can be positioned independently. To ensure it always looks like a capsule, we still need to rotate the caps to face each other (which has been done in a small script).

---
+++ HoLola/Assets/scripts/Capsule.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capsule : MonoBehaviour {
+    Transform cap1; // capsule end-caps
+    Transform cap2;
+
+	// Use this for initialization
+	void Start () {
+        
+        cap1 = FindInChildren(this.transform, "cap_1");
+        cap2 = FindInChildren(this.transform, "cap_2");
+	}
+
+    Transform FindInChildren(Transform parent, string name)
+    {
+        var result = parent.Find(name);
+        if (result != null)
+            return result;
+        foreach (Transform child in parent)
+        {
+            result = FindInChildren(child, name);
+            if (result != null)
+                return result;
+        }
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        cap1.LookAt(cap2, Vector3.up);
+        cap2.LookAt(cap1, Vector3.down);
+    }
+}
